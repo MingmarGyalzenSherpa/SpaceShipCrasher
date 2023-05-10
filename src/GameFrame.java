f13cfec4a3ed6ebc7a69be96c5b76c420be9ab19
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-05-11 00:03:08 +0545

    game frame updated

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,2 +1,15 @@
-public class GameFrame {
+import javax.swing.*;
+import java.io.IOException;
+
+public class GameFrame  extends JFrame {
+
+    GameFrame() throws IOException {
+        this.add(new GamePanel());
+        this.pack();
+        this.setLayout(null);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setTitle("Spaceship");
+        this.setVisible(true);
+    }
 }
